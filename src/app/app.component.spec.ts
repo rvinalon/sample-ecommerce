60ea83d969ee8022e327ea239d5ae8d943a86156
Ronald Vinalon <ronald.vinalon@outlook.com>
2020-03-04 21:00:15 +0800

    add new project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
 		expect(app).toBeTruthy();
 	});
 
-	it(`should have as title 'angular-tailwind'`, () => {
+	it(`should have as title 'sample-ecommerce'`, () => {
 		const fixture = TestBed.createComponent(AppComponent);
 		const app = fixture.componentInstance;
-		expect(app.title).toEqual('angular-tailwind');
+		expect(app.title).toEqual('sample-ecommerce');
 	});
 
 	it('should render title', () => {
 		const fixture = TestBed.createComponent(AppComponent);
 		fixture.detectChanges();
 		const compiled = fixture.nativeElement;
-		expect(compiled.querySelector('.content span').textContent).toContain('angular-tailwind app is running!');
+		expect(compiled.querySelector('.content span').textContent).toContain('sample-ecommerce app is running!');
 	});
 });
