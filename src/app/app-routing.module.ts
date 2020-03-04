d070337cab766b0c0a6789be555d89cc9041461c
Ronald Vinalon <ronald.vinalon@outlook.com>
2020-03-04 20:49:20 +0800

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		loadChildren: () => import('../pages/home/home.module').then(m => m.HomeModule)
+	}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
